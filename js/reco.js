57a6f409b11d08073c9ebeb652551f291e613a4a
Sylvain <SylvainBzh@users.noreply.github.com>
2017-09-18 11:51:21 +0200

    September update

--- js/reco.js
+++ js/reco.js
@@ -131,6 +131,8 @@ $(document).ready( function () {
     });
   }
   
+  // list glossary items
+  generateGlossaryLinks();
 });
 
 function setBreadcrumb(param) {
@@ -187,4 +189,240 @@ function getCurrentItem() {
   } else {
     return false;
   }
+}
+
+/*
+  Glossary
+*/
+var GlossaryTitle = {
+  "fr":"Glossaire",
+  "en":"Glossary"
+};
+var Glossary = { // collection of all terms to be defined
+  "API":{
+    "base":"<span lang='en'>Application Programming Interface</span>",
+    "fr":"interface de programmation"
+  },
+  "ARIA":{
+    "base":"<span lang='en'>Accessible Rich Internet Applications</span>",
+    "fr":"applications internet riches accessibles"
+  },
+  "AT":{
+    "base":"<span lang='en'>Assistive Technology</span>",
+    "fr":"technologie d’assistance"
+  },
+  "CSS":{
+    "base":"<span lang='en'>Cascading Stylesheets</span>",
+    "fr":"feuilles de style en cascade"
+    },
+  "DINSIC":{
+    "base":"<span lang='fr'>Direction Interministérielle du Numérique et du Système d’Information et de Communication",
+    "en":"French interministry digital and information systems direction"
+  },
+  "DOM":{
+    "base":"<span lang='en'>Document Object Model</span>",
+    "fr":"modèle d’objet document"
+  },
+  "EASE":{
+    "base":"<span lang='en'>e-Accessibility Services for Everyone</span>",
+    "fr":"service d’accessibilité numérique chez Orange"
+  },
+  "EULA":{
+    "base":"<span lang='en'>End-User Licence Agreement</span>",
+    "fr":"contrat utilisateur final"
+  },
+  "HTML":{
+    "base":"<span lang='en'>HyperText Markup Language</span>",
+    "fr":"langage de marquage hypertexte",
+    },
+  "HTML5":{
+    "base":"<span lang='en'>HyperText Markup Language version 5</span>",
+    "fr":"langage de marquage hypertexte version 5"
+    },
+  "ID":{
+    "base":"<span lang='en'>Identifier</span>",
+    "fr":"identifiant"
+  },
+  "IE":{
+    "base":"<span lang='en'>Microsoft Internet Explorer</span>",
+  },
+  "iOS":{
+    "base":"<span lang='en'>Apple’s Operating System for mobile and tablet</span>",
+    "fr":"système d’exploitation d’Apple pour mobile et tablette"
+  },
+  "IP":{
+    "base":"<span lang='en'>Internet Protocol</span>",
+    "fr":"protocole internet"
+  },
+  "ISO":{
+    "base":"<span lang='en'>International Organization for Standardization</span>",
+    "fr":"organisation internationale pour la standardisation"
+  },
+  "JAWS":{
+    "base":"<span lang='en'>Job Access With Speech</span>",
+    "fr":"lecteur d’écran"
+  },
+  "JO":{
+    "base":"<span lang='fr'>Journal Officiel</span>",
+    "en":"French official journal of laws and regulations"
+  },
+  "JS":{
+    "base":"Javascript",
+    },
+  "MIT":{
+    "base":"<span lang='en'>Massachussetts Institute of Technology</span>",
+    "fr":"institut de technologie du Massachussetts"
+  },
+  "NVDA":{
+    "base":"<span lang='en'>NonVisual Desktop Access</span>",
+    "fr":"lecteur d’écran"
+  },
+  "OSX":{
+    "base":"<span lang='en'>Apple’s Operating System version 10</span>",
+    "fr":"système d’exploitation d’Apple version 10"
+  },
+  "PC":{
+    "base":"<span lang='en'>Personal Computer</span>",
+    "fr":"ordinateur personnel"
+  },
+  "PDF":{
+    "base":"<span lang='en'>Portable Document Format</span>",
+    "fr":"format de documents portables"
+  },
+  "RGAA":{
+    "base":"<span lang='fr'>Référentiel général d’Accessibilité des Administrations</span>",
+    "en":"French Referential for the accessibility of administrations"
+    },
+  "SDK":{
+    "base":"<span lang='en'>Software Development Kit</span>",
+    "fr":"kit de développement logiciel"
+  },
+  "SEO":{
+    "base":"<span lang='en'>Search Engine Optimisation</span>",
+    "fr":"optimisation pour les moteurs de recherche"
+  },
+  "SVG":{
+    "base":"<span lang='en'>Scalable Vector Graphics</span>",
+    "fr":"graphique vectoriel adaptable"
+  },
+  "TBD":{
+    "base":"<span lang='en'>To Be Done</span>",
+    "fr":"à faire"
+  },
+  "TTS":{
+    "base":"<span lang='en'>Text To Speech</span>",
+    "fr":"conversion texte vers parole"
+  },
+  "UI":{
+    "base":"<span lang='en'>User Interface</span>",
+    "fr":"interface utilisateur"
+  },
+  "URL":{
+    "base":"<span lang='en'>Universal Resource Locator</span>",
+    "fr":"emplacement d’une ressource"
+    },
+  "W3C":{
+    "base":"<span lang='en'>World Wide Web Consortium</span>",
+    },
+  "WAI":{
+    "base":"<span lang='en'>Web Accesibility Initiative</span>",
+    "fr":"initiative pour l’accessibilité web"
+  },
+  "WAI-ARIA":{
+    "base":"<span lang='en'>Web Accesibility Initiative: Accessible Rich Internet Applications</span>",
+    "fr":"applications internet riches accessibles"
+  },
+  "WCAG":{
+    "base":"<span lang='en'>Web Content Accessibility Guidelines</span>",
+    "fr":"recommandations d’accessibilité des contenus web"
+  },
+  "XHTML":{
+    "base":"<span lang='en'>eXtensible HyperText Markup Language</span>",
+    "fr":"langage de marquage hypertexte extensible",
+    },
+  "XML":{
+    "base":"<span lang='en'>eXtensible Markup Language</span>",
+    "fr":"langage de marquage extensible",
+  },
+  "XPath":{
+    "base":"<span lang='en'>XML Path Language</span>",
+    "fr":"langage de parcours XML"
+  },
+};
+
+function generateGlossaryLinks() {
+  // find the page's language
+  var pagelang = $('HTML').attr('lang');
+  // create an object to store the glossary table
+  var GlossaryTable = [],
+    anchorprefix="glossarytable", // prefix for links
+    glossarytext = ""; // text string to generate final HTML
+  
+  // find all abbreviations
+  //$('#content :not(a) abbr').each(function(){
+  $('#content :not(a) > abbr').each(function(){
+    var $abbr = $(this), text = $abbr.text().toUpperCase(); // storing variable to go faster
+    
+    if( Glossary[text] ) { // if the innerText of the node is found in Glossary
+      var anchor = encodeURI(anchorprefix + text); // anchor that we will link to
+
+      // add a new entry in the glossary table
+      if( !Glossary[text]["alreadyset"] ) { // if not set yet
+        // generate a string with the definition
+        var str = Glossary[text]["base"];
+        if(Glossary[text][pagelang]) { // if there is complementary content in the page's language
+          str += ", " + Glossary[text][pagelang];
+        }
+
+        GlossaryTable.push({
+          "dt":text, // key text
+          "dd":str, // definition text
+          "anchor":anchor
+        });
+        Glossary[text]["alreadyset"] = true;
+      }
+
+      // wrap $abbr in a link to the glossary table entry
+      $abbr.wrap($("<a class='abbrlink' href='#" + anchor + "'>"));
+
+      // add a title to the abbr if it doesn't already have one
+      if ( !$abbr.attr('title') ) {
+        $abbr.attr('title', $(Glossary[text]["base"]).text() );
+      }
+
+    }
+
+
+  });
+
+  // if glossary is empty => exit  
+  if (!!!GlossaryTable.length) {    
+    return true;
+  }
+
+  // generate glossary HTML
+  // first, sort result table
+  GlossaryTable.sort(function(a, b) {
+    var strA = a.dt;
+    var strB = b.dt;
+    if (strA < strB) {
+      return -1;
+    }
+    if (strA > strB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+
+  // generate glossary text
+  for(var i=0; i<GlossaryTable.length; i++) {
+    glossarytext += "<dt id='" + GlossaryTable[i]["anchor"] + "'>" + GlossaryTable[i]["dt"] + "</dt>";
+    glossarytext += "<dd>" + GlossaryTable[i]["dd"] + "</dd>";
+  };
+  // append glossary to #content
+  var glossarytitle = (GlossaryTitle[pagelang]) ? GlossaryTitle[pagelang] : GlossaryTitle.en;
+  glossarytext = "<div id='glossary' role='complementary'><h2>" + glossarytitle + "</h2><dl>" + glossarytext + "</dl></div>";
+  $('#content').append(glossarytext);
 }
--- others/lunr-index.js
+++ others/lunr-index.js
@@ -194,7 +194,7 @@ documentTitles["powerpoint.html#graphiques-avec-textes-de-remplacement"] = "Grap
 index.add({
     url: "powerpoint.html#graphiques-avec-textes-de-remplacement",
     title: "Graphiques avec textes de remplacement",    
-    body: "### Graphiques avec textes de remplacement Comportement attendu pour Jaws et &lt;abbr&gt;NVDA&lt;/abbr&gt;, le texte de remplacement est vocalisé à la place du graphique.   &amp;nbsp; &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
+    body: "### Graphiques avec textes de remplacement Comportement attendu pour Jaws et &lt;abbr&gt;NVDA&lt;/abbr&gt;, le texte de remplacement est vocalisé à la place du graphique.  &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
 });
 
 
@@ -343,7 +343,7 @@ documentTitles["word.html#ressources-externes"] = "Ressources externes";
 index.add({
     url: "word.html#ressources-externes",
     title: "Ressources externes",    
-    body: "## Ressources externes  - [Créer des documents bureautiques accessibles](https://disic.github.io/guides-documents_bureautiques_accessibles/html/), &lt;abbr title=\&quot;direction interministérielle du numérique et du système d’information et de communication\&quot;&gt;DINSIC&lt;/abbr&gt; (français). - &lt;a href=\&quot;http://webaim.org/techniques/word/\&quot; hreflang=\&quot;en\&quot;&gt;Accessibilité Word, WebAIM&lt;/a&gt; (anglais).  &amp;nbsp; &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
+    body: "## Ressources externes  - [Créer des documents bureautiques accessibles](https://disic.github.io/guides-documents_bureautiques_accessibles/html/), &lt;abbr title=\&quot;direction interministérielle du numérique et du système d’information et de communication\&quot;&gt;DINSIC&lt;/abbr&gt; (français). - &lt;a href=\&quot;http://webaim.org/techniques/word/\&quot; hreflang=\&quot;en\&quot;&gt;Accessibilité Word, WebAIM&lt;/a&gt; (anglais).  &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
 });
 
 
@@ -545,7 +545,28 @@ documentTitles["editorial.html#introduction"] = "Introduction";
 index.add({
     url: "editorial.html#introduction",
     title: "Introduction",    
-    body: "## Introduction Cette section est en cours de rédaction et sera disponible très prochainement.  &amp;nbsp; &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
+    body: "## Introduction Cette section propose des recommandations générales à respecter pour garantir l’accessibilité de vos contenus quel que soit le support utilisé (email, Web, Word, etc.).  "
+});
+
+documentTitles["editorial.html#verifier-les-couleurs"] = "Vérifier les couleurs";
+index.add({
+    url: "editorial.html#verifier-les-couleurs",
+    title: "Vérifier les couleurs",    
+    body: "## Vérifier les couleurs  - S’assurer que la couleur n’est pas le seul moyen utilisé pour communiquer l’information. - Assurer un niveau de contraste suffisant entre la couleur du texte et celle de l’arrière-plan.    Le contraste peut être vérifié à l’aide de l’outil &lt;a href=\&quot;https://www.paciellogroup.com/resources/contrastanalyser/\&quot; hreflang=\&quot;en\&quot; lang=\&quot;en\&quot;&gt;Colour Contrast Analyzer&lt;/a&gt;&amp;nbsp;:  - 4.5:1 pour du texte de taille normale.  - 3:1 pour du texte de grande taille.  "
+});
+
+documentTitles["editorial.html#faciliter-la-lecture"] = "Faciliter la lecture";
+index.add({
+    url: "editorial.html#faciliter-la-lecture",
+    title: "Faciliter la lecture",    
+    body: "## Faciliter la lecture  -	Aligner le texte à gauche, mais ne pas le justifier. -	Utiliser une police de caractères facilement lisible en corps 12 pixels minimum. -	Mettre en place une ponctuation simple. -	Ne pas mettre d’italique, ni de phrase en majuscule, mais une majuscule en début de phrase. -	Éviter les fonds texturés (images). -	Ne pas faire de retours chariots répétés ou de tabulation pour espacer (mais la fonction «&amp;nbsp;Paragraphe&amp;nbsp;&amp;gt; Espacement&amp;nbsp;» ou «&amp;nbsp;Retrait&amp;nbsp;» dans Word, par exemple). -	Éviter les tableaux de mise en page ou de données. -	Éviter le contenu image porteur d’information&amp;nbsp;: graphes, schémas, diagrammes… -	Mettre en place un indicateur de fin de document (par exemple, une image indiquant «&amp;nbsp;fin de mail&amp;nbsp;», un texte blanc sur fond blanc «&amp;nbsp;fin de document&amp;nbsp;»). -	Utiliser des tournures de phrases limpides, des phrases courtes et à la construction simple&amp;nbsp;: sujet, verbe, complément. Une idée par phrase, des mots simples. -	Expliquer les mots compliqués. -	Utiliser le présent, de préférence le style direct et la forme active (pas de subjonctif, pas de conjonction). -	Utiliser les caractères typographiques du Français&amp;nbsp;: tiret - , cadratin — , «&amp;nbsp;guillemets français&amp;nbsp;». -	Ajouter des accents même aux majuscules éèêçàïùÉÈÊÇÀÏÙ («&amp;nbsp;Insertion&amp;nbsp;&amp;gt; Symbole&amp;nbsp;&amp;gt; Texte normal&amp;nbsp;»).  -	Éviter les termes dans une langue étrangère (mots en anglais), les sigles ou abréviations sauf ceux passés dans le langage courant.  "
+});
+
+documentTitles["editorial.html#fournir-une-mise-en-page-des-tableaux-accessibles"] = "Fournir une mise en page des tableaux accessibles";
+index.add({
+    url: "editorial.html#fournir-une-mise-en-page-des-tableaux-accessibles",
+    title: "Fournir une mise en page des tableaux accessibles",    
+    body: "## Fournir une mise en page des tableaux accessibles  Simplifier les tableaux sachant que les clients de messagerie (Outlook inclus) obligent encore parfois à utiliser une mise en page en tableau.  À vérifier&amp;nbsp;:   - Globalement, ne pas utiliser de mise en page complexe de tableau (fusion de cellules d’un tableau, multicolonnage…). - Éviter d’imbriquer les tableaux de mise en page (grand maximum, 2 niveaux d’imbrication), mais aussi&amp;nbsp;: - Ne pas fusionner les cellules d’un tableau, mais utiliser plusieurs tableaux plus simples. - Ne pas conserver de cellules vides. - Ne pas ajouter d’images de fond. - Assurer une lecture séquentielle (on doit pouvoir lire de gauche à droite et de haut en bas). - Éviter les tableaux de données, mais aussi les graphiques ou les schémas complexes.  &amp;nbsp; &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
 });
 
 
