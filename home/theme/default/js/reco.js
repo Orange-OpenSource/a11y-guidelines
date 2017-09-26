f1e6133fa134ff7002724972cf141a8a67268ca6
Sylvain <SylvainBzh@users.noreply.github.com>
2017-09-26 14:23:49 +0200

    Remove old CDU code

--- home/theme/default/js/reco.js
+++ home/theme/default/js/reco.js
@@ -83,10 +83,6 @@ $(document).ready( function () {
     return window.print();
   }
 
-  $("#cdu a").on("click", function () {
-    $("#cdu_close button").click();
-  });
-
   // Lien retour en haut de page
   $(window).scroll(function () {
     if (app.scrollTimer) {
