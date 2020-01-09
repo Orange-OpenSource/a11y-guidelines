18e3a5ec84c843414dbee5dfd66da9d4e161ee50
Pigeard Sylvain <13221271+PigeardSylvain@users.noreply.github.com>
2020-01-09 11:31:38 +0100

    aria-live and alert role

--- web/lunr-index.js
+++ web/lunr-index.js
@@ -190,6 +190,50 @@ index.add({
 
 
 
+documentTitles["aria-live-alert.html#lattribut-aria-live-et-le-role-alert"] = "L'attribut aria-live et le role alert";
+index.add({
+    url: "aria-live-alert.html#lattribut-aria-live-et-le-role-alert",
+    title: "L'attribut aria-live et le role alert",    
+    body: "# L'attribut aria-live et le role alert    &lt;script&gt;$(document).ready(function () {     setBreadcrumb([             {\&quot;label\&quot;:\&quot;Articles techniques\&quot;, \&quot;url\&quot;:\&quot;techniques.html\&quot;},             {\&quot;label\&quot;:\&quot;L'attribut aria-live et le role alert\&quot;}         ]); }); &lt;/script&gt;    Les utilisateurs qui naviguent à l'aide d'un lecteur d'écran ne sont pas toujours au courant des modifications apportées dans la page. Lorsqu'une information est actualisée ou lorsqu'un message apparaît il est parfois nécessaire de faire parler le lecteur d'écran pour informer l'utilisateur. Pour ce faire, le language ARIA dispose du role `alert` et de l'attribut `aria-live`.  "
+});
+
+documentTitles["aria-live-alert.html#le-role-alert"] = "Le role alert";
+index.add({
+    url: "aria-live-alert.html#le-role-alert",
+    title: "Le role alert",    
+    body: "## Le role alert  Positionné sur un élément HTML, celui-ci permet d'indiquer au lecteur d'écran de vocaliser l'élément automatiquement lors de sa création. Veiller toutefois à utiliser ce role uniquement dans les cas appropriés comme il est clairement indiqué dans la documentation [MDN de Mozilla](https://developer.mozilla.org/fr/docs/Accessibilit%C3%A9/ARIA/Techniques_ARIA/Utiliser_le_r%C3%B4le_alert). De fait de sa nature intrusive, le role alert doit être utilisé avec parcimonie et uniquement dans les situations où l’attention de l’utilisateur est immédiatement requise. Les changements dynamiques de moindre urgence devraient utiliser une méthode moins agressive, telle que aria-live=\&quot;polite\&quot; ou autres rôles de zone live.  Pour déclencher une alerte, plusieurs méthodes sont possibles, avec des supports qui différent en fonction du couple navigateur et lecteur d'écran utilisé. Vous pouvez consulter [l'article de Steve Faulkner (en anglais)](https://developer.paciellogroup.com/blog/2017/04/aria-alert-support/) à ce sujet.  Voici quelques exemples de méthodes qui sont bien supportées.  "
+});
+
+documentTitles["aria-live-alert.html#creer-un-nouvel-element-dans-le-dom"] = "Créer un nouvel élément dans le DOM";
+index.add({
+    url: "aria-live-alert.html#creer-un-nouvel-element-dans-le-dom",
+    title: "Créer un nouvel élément dans le DOM",    
+    body: "### Créer un nouvel élément dans le DOM On peut déclencher une alerte en insérant un nouvel élément dans le DOM via Javascript.  &lt;pre&gt;&lt;code class=\&quot;html\&quot;&gt;&amp;lt;span role=\&quot;alert\&quot;&amp;gt;Ceci est un message d'alerte.&amp;lt;/span&amp;gt;&lt;/code&gt;&lt;/pre&gt;  "
+});
+
+documentTitles["aria-live-alert.html#ajouter-un-role-alert-sur-un-element-existant"] = "Ajouter un role alert sur un élément existant";
+index.add({
+    url: "aria-live-alert.html#ajouter-un-role-alert-sur-un-element-existant",
+    title: "Ajouter un role alert sur un élément existant",    
+    body: "### Ajouter un role alert sur un élément existant Le déclenchement d'une alerte peut également s'effectuer en ajoutant un role alert à un élément existant dynamiquement via Javascript.  &lt;pre&gt;&lt;code class=\&quot;js\&quot;&gt; document.getElementById('alert').addAttribute(\&quot;role\&quot;, \&quot;alert\&quot;); &lt;/code&gt;&lt;/pre&gt;  "
+});
+
+documentTitles["aria-live-alert.html#utiliser-innerhtml"] = "Utiliser innerHTML";
+index.add({
+    url: "aria-live-alert.html#utiliser-innerhtml",
+    title: "Utiliser innerHTML",    
+    body: "### Utiliser innerHTML  Créer une alerte via la propriété innerHTML. &lt;pre&gt;&lt;code class=\&quot;js\&quot;&gt; element.innerHTML = '&amp;lt;div role=\&quot;alert\&quot;&amp;gt;Ceci est une alerte&amp;lt;/div&amp;gt'; &lt;/code&gt;&lt;/pre&gt;  "
+});
+
+documentTitles["aria-live-alert.html#lattribut-aria-live"] = "L'attribut aria-live";
+index.add({
+    url: "aria-live-alert.html#lattribut-aria-live",
+    title: "L'attribut aria-live",    
+    body: "## L'attribut aria-live  Positionné sur un élément HTML, l'attribut aria-live permet d'indiquer au lecteur d'écran que toute modification apportée à son contenu entraînera une vocalisation par le lecteur d'écran.    Trois valeurs sont possibles :  - Off : aucune vocalisation - Polite : la vocalisation aura lieu lorsque le lecteur d'écran aura fini la tâche en cours - Assertive : le lecteur d'écran interrompt la tâche en cours pour informer l'utilisateur  &lt;pre&gt;&lt;code class=\&quot;html\&quot;&gt; &amp;lt;span aria-live=\&quot;polite\&quot;&amp;gt;5 éléments sélectionnés&amp;lt/span&amp;gt; &lt;/code&gt;&lt;/pre&gt;   &amp;nbsp;      &lt;!--  This file is part of a11y-guidelines | Our vision of mobile &amp; web accessibility guidelines and best practices, with valid/invalid examples.  Copyright (C) 2016  Orange SA  See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file). --&gt;"
+});
+
+
+
 documentTitles["aria-status.html#utiliser-abbrariaabbr-les-messages-detat-de-statut-ou-contextuel"] = "Utiliser &lt;abbr&gt;ARIA&lt;/abbr&gt;  les messages d'état, de statut ou contextuel";
 index.add({
     url: "aria-status.html#utiliser-abbrariaabbr-les-messages-detat-de-statut-ou-contextuel",
