19321948f7681eec3ff4dbbcdf1f7439685c3e8e
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-05-13 16:31:13 +0200

    refactor(examples): French web examples now follow the 11ty content architecture

--- src/fr/web/exemples-de-composants/formulaires/script.js
+++ src/fr/web/exemples-de-composants/formulaires/script.js
@@ -1,16 +1,13 @@
-/*!
-* This file is part of a11y-guidelines | Our vision of mobile & web accessibility guidelines and best practices, with valid/invalid examples.
-* Copyright (C) 2016  Orange SA
-* See the Creative Commons Legal Code Attribution-ShareAlike 3.0 Unported License for more details (LICENSE file).**/
 $(document).ready(function() {
+
       jQuery.extend(jQuery.validator.messages, {
             required: "Veuillez renseigner le champ : {0}."
       });
-      
-	jQuery("#formulaires").validate({
-            errorContainer: "#globalErrorMessage",            
 
-            rules: {                  
+	jQuery("#formulaires").validate({
+            errorContainer: "#globalErrorMessage",
+
+            rules: {
                   "serie":{
                         "required": true,
                         "regex": /TS-\d{4}/
@@ -35,13 +32,13 @@ $(document).ready(function() {
                   }
                   return this.optional(element) || regexp.test(value);
             },
-            function (regex, input) {                  
+            function (regex, input) {
                   return jQuery.validator.format("Le format du champ {0}, n'est pas valide.", $(input).attr("data-displayname"));
             }
       );
-      	 
-     $.validator.messages.required = function (param, input) {           
+
+     $.validator.messages.required = function (param, input) {
             return 'Le champ ' + $(input).attr("data-displayname") + ' est obligatoire.';
      }
-     
+
 });
