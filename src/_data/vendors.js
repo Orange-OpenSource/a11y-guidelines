4fa796f37d626ad48887edf8dab364934d70fff0
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-05-13 17:09:32 +0200

    refactor(vendors): make it easier to update vendors

---
+++ src/_data/vendors.js
@@ -0,0 +1,11 @@
+module.exports = {
+  boosted: {
+    favicon: {
+      version: '4.5'
+    },
+    css: {
+      version: '4.5.0',
+      integrity: 'sha384-QYLV+ojryK3B49TCQMGtvBTwDYQmQzzuiw3Ajw+9I4GkTu6bV3bl/YNUv/0IY5hg'
+    }
+  }
+}
