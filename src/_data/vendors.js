0a67ac20b1095a8c31caa716155219317c5b0283
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-05-19 17:09:53 +0200

    refactor(vendors): store the boosted version independently of the language

--- src/_data/vendors.js
+++ src/_data/vendors.js
@@ -1,10 +1,10 @@
 module.exports = {
   boosted: {
+    version: '4.5.0',
     favicon: {
       version: '4.5'
     },
     css: {
-      version: '4.5.0',
       integrity: 'sha384-QYLV+ojryK3B49TCQMGtvBTwDYQmQzzuiw3Ajw+9I4GkTu6bV3bl/YNUv/0IY5hg'
     }
   }
