3fd07bc7353319bdeaa9572b2f1b9d8095483ede
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-06-11 16:38:21 +0200

    feat(jquery): add the posibility to load jQuery when needed

--- src/_data/vendors.js
+++ src/_data/vendors.js
@@ -7,5 +7,9 @@ module.exports = {
     css: {
       integrity: 'sha384-QYLV+ojryK3B49TCQMGtvBTwDYQmQzzuiw3Ajw+9I4GkTu6bV3bl/YNUv/0IY5hg'
     }
+  },
+  jquery: {
+    version: '3.5.1',
+    integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj'
   }
 }
