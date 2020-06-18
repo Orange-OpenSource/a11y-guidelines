0a24eef9da2461b53877873557bccd14826f8481
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-06-18 10:10:23 +0200

    feat(vendors): add HighlightJS & enable jQuery for components examples

--- src/_data/vendors.js
+++ src/_data/vendors.js
@@ -8,6 +8,10 @@ module.exports = {
       integrity: 'sha384-QYLV+ojryK3B49TCQMGtvBTwDYQmQzzuiw3Ajw+9I4GkTu6bV3bl/YNUv/0IY5hg'
     }
   },
+  highlightjs: {
+    version: '10.1.1',
+    theme: 'github'
+  },
   jquery: {
     version: '3.5.1',
     integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj'
