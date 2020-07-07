e0092d366c09dbe93a97b51b8252d90710c20416
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-07-07 16:17:13 +0200

    refactor(toc): Use Boosted's scrollspy for our TOC
    Also, include Boosted's JavaScript conditionnally and display the TOC on
    posts

--- src/_data/vendors.js
+++ src/_data/vendors.js
@@ -3,6 +3,9 @@ module.exports = {
     version: '4.5.0',
     css: {
       integrity: 'sha384-QYLV+ojryK3B49TCQMGtvBTwDYQmQzzuiw3Ajw+9I4GkTu6bV3bl/YNUv/0IY5hg'
+    },
+    js: {
+      integrity: 'sha384-R024d8eLkqTtaLUSJ9MrQ9ay65hb/spmrvV7W1B2MuguSjYsCx3KVKjdjc9zUbPb'
     }
   },
   highlightjs: {
