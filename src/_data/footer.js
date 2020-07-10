79485092a19cb369d82411390c0a4dc60b059baa
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-07-10 11:30:25 +0200

    refactor(data): gather all navigations' data in one data file

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -1,16 +1,4 @@
 module.exports = {
-  navigation: {
-    en: [
-      { label: 'Contact', href: '/en/contact' },
-      { label: 'About', href: '/en/about' },
-      { label: 'Sitemap', href: '/en/sitemap/' },
-    ],
-    fr: [
-      { label: 'Contact', href: '/fr/contact' },
-      { label: 'À propos', href: '/fr/a-propos' },
-      { label: 'Plan de site', href: '/fr/plan-de-site/' },
-    ]
-  },
   openIssue: {
     en: {
       label: 'Report an error on Github<span class="sr-only">&nbsp;(new window)</span>',
