d8dfd559e15def8175ec14d1d3836ef41d063dd4
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-08-04 15:59:42 +0200

    feat(glossary): Import raw glossary data file

---
+++ src/_data/glossary.js
@@ -0,0 +1,179 @@
+module.exports = {
+  "AIDES TECHNIQUES":{
+    "base":"<span lang='fr'>Une aide technique est un matériel ou logiciel utilisé par une personne en situation de handicap destiné à compenser, limiter ou neutraliser la déficience.</span>",
+    "fr":""
+  },
+  "ASSISTIVE TECHNOLOGIES":{
+    "base":"<span lang='en'>Assistive technologies are any product or software that is used to increase, maintain, or improve the functional capabilities of people with disabilities. Ergonomic keyboards, screen readers, magnifier and switch devices are examples.</span>",
+    "fr":""
+  },
+  "API":{
+    "base":"<span lang='en'>Application Programming Interface</span>",
+    "fr":"interface de programmation"
+  },
+  "ARIA":{
+    "base":"<span lang='en'>Accessible Rich Internet Applications</span>",
+    "fr":"applications internet riches accessibles"
+  },
+  "AT":{
+    "base":"<span lang='en'>Assistive Technology</span>",
+    "fr":"technologie d’assistance"
+  },
+  "CSS":{
+    "base":"<span lang='en'>Cascading Stylesheets</span>",
+    "fr":"feuilles de style en cascade"
+  },
+  "DINSIC":{
+    "base":"<span lang='fr'>Direction Interministérielle du Numérique et du Système d’Information et de Communication",
+    "en":"French interministry digital and information systems direction"
+  },
+  "DOM":{
+    "base":"<span lang='en'>Document Object Model</span>",
+    "fr":"modèle d’objet document"
+  },
+  "EASE":{
+    "base":"<span lang='en'>e-Accessibility Solutions for Everyone</span>",
+    "fr":"service d’accessibilité numérique chez Orange"
+  },
+  "EULA":{
+    "base":"<span lang='en'>End-User Licence Agreement</span>",
+    "fr":"contrat utilisateur final"
+  },
+  "HTML":{
+    "base":"<span lang='en'>HyperText Markup Language</span>",
+    "fr":"langage de marquage hypertexte",
+  },
+  "HTML5":{
+    "base":"<span lang='en'>HyperText Markup Language version 5</span>",
+    "fr":"langage de marquage hypertexte version 5"
+  },
+  "ID":{
+    "base":"<span lang='en'>Identifier</span>",
+    "fr":"identifiant"
+  },
+  "IDE":{
+    "base":"<span lang='en'>Integrated Development Environment</span>",
+    "fr":"environnement de développement intégré"
+  },
+  "IE":{
+    "base":"<span lang='en'>Microsoft Internet Explorer</span>",
+  },
+  "iOS":{
+    "base":"<span lang='en'>Apple’s Operating System for mobile and tablet</span>",
+    "fr":"système d’exploitation d’Apple pour mobile et tablette"
+  },
+  "IP":{
+    "base":"<span lang='en'>Internet Protocol</span>",
+    "fr":"protocole internet"
+  },
+  "IPA":{
+    "base":"<span lang='en'>International Phonetic Alphabet</span>",
+  },
+  "ISO":{
+    "base":"<span lang='en'>International Organization for Standardization</span>",
+    "fr":"organisation internationale pour la standardisation"
+  },
+  "JAWS":{
+    "base":"<span lang='en'>Job Access With Speech</span>",
+    "fr":"lecteur d’écran"
+  },
+  "JO":{
+    "base":"<span lang='fr'>Journal Officiel</span>",
+    "en":"French official journal of laws and regulations"
+  },
+  "JS":{
+    "base":"Javascript",
+  },
+  "KOTLIN":{
+    "base":"<span lang='en'>Kotlin langage</span>",
+    "en":"Kotlin is the second langage used for programming on Android devices",
+    "fr":"Kotlin est le second langage utilisé pour programmer sur les mobiles Android"
+  },
+  "MIT":{
+    "base":"<span lang='en'>Massachussetts Institute of Technology</span>",
+    "fr":"institut de technologie du Massachussetts"
+  },
+  "NVDA":{
+    "base":"<span lang='en'>NonVisual Desktop Access</span>",
+    "fr":"lecteur d’écran"
+  },
+  "OSX":{
+    "base":"<span lang='en'>Apple’s Operating System version 10</span>",
+    "fr":"système d’exploitation d’Apple version 10"
+  },
+  "PC":{
+    "base":"<span lang='en'>Personal Computer</span>",
+    "fr":"ordinateur personnel"
+  },
+  "PDF":{
+    "base":"<span lang='en'>Portable Document Format</span>",
+    "fr":"format de documents portables"
+  },
+  "RGAA":{
+    "base":"<span lang='fr'>Référentiel général d’Amélioration de l'Accessibilité</span>",
+    "en":"French referential for improving accessibility"
+  },
+  "SDK":{
+    "base":"<span lang='en'>Software Development Kit</span>",
+    "fr":"kit de développement logiciel"
+  },
+  "SEO":{
+    "base":"<span lang='en'>Search Engine Optimisation</span>",
+    "fr":"optimisation pour les moteurs de recherche"
+  },
+  "SVG":{
+    "base":"<span lang='en'>Scalable Vector Graphics</span>",
+    "fr":"graphique vectoriel adaptable"
+  },
+  "TBD":{
+    "base":"<span lang='en'>To Be Done</span>",
+    "fr":"à faire"
+  },
+  "TDD":{
+    "base":"<span lang='en'>Test Driven Development</span>",
+    "fr":"tests unitaires"
+  },
+  "TICS":{
+    "base":"<span lang='fr'>Technologies de l'Information et de la Communication</span>",
+    "fr":""
+  },
+  "TTS":{
+    "base":"<span lang='en'>Text To Speech</span>",
+    "fr":"conversion texte vers parole"
+  },
+  "UI":{
+    "base":"<span lang='en'>User Interface</span>",
+    "fr":"interface utilisateur"
+  },
+  "URL":{
+    "base":"<span lang='en'>Universal Resource Locator</span>",
+    "fr":"emplacement d’une ressource"
+  },
+  "W3C":{
+    "base":"<span lang='en'>World Wide Web Consortium</span>",
+  },
+  "WAI":{
+    "base":"<span lang='en'>Web Accesibility Initiative</span>",
+    "fr":"initiative pour l’accessibilité web"
+  },
+  "WAI-ARIA":{
+    "base":"<span lang='en'>Web Accesibility Initiative: Accessible Rich Internet Applications</span>",
+    "fr":"applications internet riches accessibles"
+  },
+  "WCAG":{
+    "base":"<span lang='en'>Web Content Accessibility Guidelines</span>",
+    "fr":"recommandations d’accessibilité des contenus web"
+  },
+  "XHTML":{
+    "base":"<span lang='en'>eXtensible HyperText Markup Language</span>",
+    "fr":"langage de marquage hypertexte extensible",
+  },
+  "XML":{
+    "base":"<span lang='en'>eXtensible Markup Language</span>",
+    "fr":"langage de marquage extensible",
+  },
+  "XPath":{
+    "base":"<span lang='en'>XML Path Language</span>",
+    "fr":"langage de parcours XML"
+  },
+}
