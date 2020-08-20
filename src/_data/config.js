417099e4fcf2b81af2365607d9a6eb9aa16a3a82
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-08-20 16:19:30 +0200

    feat(img): make sure markdown images don't overflow their container (img-fluid)

--- src/_data/config.js
+++ src/_data/config.js
@@ -7,6 +7,14 @@ module.exports = {
       breaks: true,
       linkify: true
     },
+    markdownItCustomParser: function (md) {
+      const defaultRender = md.renderer.rules.image
+
+      md.renderer.rules.image = function (tokens, idx, options, env, self) {
+        tokens[idx].attrPush(['class', 'img-fluid'])
+        return defaultRender(tokens, idx, options, env, self)
+      }
+    },
     markdownItAnchor: {
       level: 2,
       permalink: true,
