edaada6cff10cd48d1d771192515b32d508cb75a
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-08-25 17:32:27 +0200

    feat(popper): we can now load PopperJS if we need to

--- src/_data/vendors.js
+++ src/_data/vendors.js
@@ -15,5 +15,9 @@ module.exports = {
   jquery: {
     version: '3.5.1',
     integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj'
+  },
+  popper: {
+    version: '1.16.1',
+    integrity: 'sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN'
   }
 }
