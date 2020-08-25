38836173f51b6be22fc12717b66d63a279502d39
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-08-25 16:58:53 +0200

    fix(js): modal was not setting correct focus. Also made small refactor

--- src/en/web/components-examples/dialogs/2/script.js
+++ src/en/web/components-examples/dialogs/2/script.js
@@ -1,30 +1,41 @@
 $(document).ready(function() {
+  const openButton = $('#openButton')
+  const dialog = $('#dialog')
+  const options = $("[role=option]")
+
+  function showDialog() {
+    dialog.show();
+
+    // If no selected element, select the first by default
+    if (!dialog.find("[aria-selected=true]").length) {
+      dialog.find("[role=option]:first").attr("aria-selected", "true").focus();
+    } else {
+      dialog.find("[aria-selected=true]").focus();
+    }
+  }
+
+  function hideDialog () {
+    openButton.focus();
+    dialog.hide()
+  }
 
   // --- Exemple final ---
   // Ouverture de l'exemple final
-  $('#openButton').on('click', function () {
+  openButton.on('click', function () {
     $('[role=dialog], .dialog').hide();
-    $('#dialog').show();
-
-    // Sauvegarde le focus pour le repositionner à la fermeture
-    $('#dialog').attr('data-focusBack', 'openButton');
-
-    // Place le focus sur l'élément par défaut
-    $('#dialog .defaultFocus').focus();
+    showDialog();
   });
 
   // Gestion de la touche échap
-  $('#dialog').on('keydown', function (e) {
-    if (e.keyCode == 27) {
-      $('#'+$("#dialog").attr('data-focusBack')).focus();
-      $("#dialog").hide();
+  dialog.on('keydown', function (e) {
+    if (e.keyCode === 27) {
+      hideDialog();
     }
   });
 
   // Boutons de fermeture des dialog
   $('[role=dialog] button').on('click', function () {
-    $('#'+$("#dialog").attr('data-focusBack')).focus();
-    $("#dialog").hide();
+    hideDialog();
   })
 
   $('.dialog-start').on('focus', function () {
@@ -34,74 +45,61 @@ $(document).ready(function() {
   $('.dialog-stop').on('focus', function () {
     $(this).parent().find('button:first').focus();
   });
-  
+
   // --- Listbox ---
-  // On focus
-  $("[role=listbox]").on("focus", function () {
-      // If no selected element, select the first by default
-      if (!$(this).find("[aria-selected=true]").length) {               
-          $(this).find("[role=option]:first").attr("aria-selected", "true").focus();
-      } else {
-          $(this).find("[aria-selected=true]").focus();
-      }
-  });
-  
   // On keydown
-  $("[role=listbox]").on("keydown", function (e) {            
-      var currentItem = $(this).find("[aria-selected=true]");          
-      switch (e.keyCode) {
-          case 38:  // Up arrow
-              if (currentItem.prev().length) {
-                  currentItem.attr("aria-selected", "false");                    
-                  currentItem.prev().attr("aria-selected", "true").focus();
-              }                    
-              e.preventDefault();
-              break;
-          case 40: // Down arrow          
-              if (currentItem.next().length) {
-                  currentItem.attr("aria-selected", "false");
-                  currentItem.next().attr("aria-selected", "true").focus();
-              }
-              e.preventDefault();
-              break;
-          case 13: // Enter
-              $('#'+$("#dialog").attr('data-focusBack')).focus();
-              $("#dialog").hide();
-              e.preventDefault();
-              break;
-          case 36: // home                    
-              currentItem.attr("aria-selected", "false");                    
-              currentItem.parent().find("[role=option]").first().attr("aria-selected", "true").focus();
-              e.preventDefault();
-              break;                                                            
-          case 35: // end
-              currentItem.attr("aria-selected", "false");                    
-              currentItem.parent().find("[role=option]").last().attr("aria-selected", "true").focus();
-              e.preventDefault();
-              break;                   
-      }
+  $("[role=listbox]").on("keydown", function (e) {
+    var currentItem = $(this).find("[aria-selected=true]");
+    switch (e.keyCode) {
+      case 38:  // Up arrow
+        if (currentItem.prev().length) {
+          currentItem.attr("aria-selected", "false");
+          currentItem.prev().attr("aria-selected", "true").focus();
+        }
+        e.preventDefault();
+        break;
+      case 40: // Down arrow
+        if (currentItem.next().length) {
+          currentItem.attr("aria-selected", "false");
+          currentItem.next().attr("aria-selected", "true").focus();
+        }
+        e.preventDefault();
+        break;
+      case 13: // Enter
+        hideDialog();
+        e.preventDefault();
+        break;
+      case 36: // home
+        currentItem.attr("aria-selected", "false");
+        currentItem.parent().find("[role=option]").first().attr("aria-selected", "true").focus();
+        e.preventDefault();
+        break;
+      case 35: // end
+        currentItem.attr("aria-selected", "false");
+        currentItem.parent().find("[role=option]").last().attr("aria-selected", "true").focus();
+        e.preventDefault();
+        break;
+    }
   });
-  
+
   // double click
-  $("[role=option]").on("dblclick", function (e) {
-    $('#'+$("#dialog").attr('data-focusBack')).focus();
-    $("#dialog").hide();
+  options.on("dblclick", function (e) {
+    hideDialog();
   });
 
   // Mouse down
-  $("[role=option]").on("mousedown", function (e) {
-      $(this).parent().find("[aria-selected=true]").attr("aria-selected", "false");
-      $(this).attr("aria-selected", "true").focus();
-      e.preventDefault();
+  options.on("mousedown", function (e) {
+    $(this).parent().find("[aria-selected=true]").attr("aria-selected", "false");
+    $(this).attr("aria-selected", "true").focus();
+    e.preventDefault();
   });
-  
+
   // On focus option
-  $("[role=option]").on("focus", function (e) {
-      $(this).parent().attr("tabindex", "-1");
+  options.on("focus", function (e) {
+    $(this).parent().attr("tabindex", "-1");
   });
-  
-  $("[role=option]").on("blur", function (e) {    
-      $(this).parent().attr("tabindex", "0");
+
+  options.on("blur", function (e) {
+    $(this).parent().attr("tabindex", "0");
   });
-  
 });
