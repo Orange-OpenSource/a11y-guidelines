b275da91c053a278d9f3bb1d06d2da81bba71b1f
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-08-27 14:20:43 +0200

    feat(cookies): implement tarteaucitron

--- src/_data/site.js
+++ src/_data/site.js
@@ -18,5 +18,26 @@ module.exports = {
     lastPosts: {
       limit: 6
     }
+  },
+  tracking: {
+    googletagmanagerId: "GTM-XXXX",
+    tarteaucitronConfig: {
+      "privacyUrl": "", /* Privacy policy url */
+      "hashtag": "#cookie-consent", /* Open the panel with this hashtag */
+      "cookieName": "cookie-consent", /* Cookie name */
+      "orientation": "bottom", /* Banner position (top - bottom) */
+      "showAlertSmall": false, /* Show the small banner on bottom right */
+      "cookieslist": true, /* Show the cookie list */
+      "adblocker": false, /* Show a Warning if an adblocker is detected */
+      "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+      "highPrivacy": true, /* Disable auto consent */
+      "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+      "removeCredit": false, /* Remove credit link */
+      "moreInfoLink": true, /* Show more info link */
+      "useExternalCss": true, /* If false, the tarteaucitron.css file will be loaded */
+      //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+      "readmoreLink": "/cookiespolicy", /* Change the default readmore link */
+      "mandatory": false, /* Show a message about mandatory cookies */
+    }
   }
 }
