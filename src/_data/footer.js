8d5c5dd4bae4c7116fab5ebbb8df6dffe02ab8c3
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2021-03-10 09:55:28 +0100

    fix(typo): H in GitHub has to be capitalized

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -1,12 +1,12 @@
 module.exports = {
   openIssue: {
     en: {
-      label: 'Report an error on Github<span class="sr-only">&nbsp;(new window)</span>',
-      title: 'Report an error on Github (new window)'
+      label: 'Report an error on GitHub<span class="sr-only">&nbsp;(new window)</span>',
+      title: 'Report an error on GitHub (new window)'
     },
     fr: {
-      label: 'Signaler une erreur sur Github<span class="sr-only">&nbsp;(nouvelle fenêtre)</span>',
-      title: 'Signaler une erreur sur Github (nouvelle fenêtre)'
+      label: 'Signaler une erreur sur GitHub<span class="sr-only">&nbsp;(nouvelle fenêtre)</span>',
+      title: 'Signaler une erreur sur GitHub (nouvelle fenêtre)'
     }
   }
 }
