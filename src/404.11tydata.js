028ecc94aa9b91a34020b7bc28417ba98e00244e
DANET Julien <NTLN2278@ad-subs.w2k.francetelecom.fr>
2021-04-30 09:45:31 +0200

    fix(content): add boostedJS in 404 page

--- src/404.11tydata.js
+++ src/404.11tydata.js
@@ -2,5 +2,6 @@ const helpers = require('./_data/helpers')
 const site = require('./_data/site')
 
 module.exports = {
-  locale: helpers.getDefaultLocale(site.locales, 'code')
+  locale: helpers.getDefaultLocale(site.locales, 'code'),
+  boostedJS: true
 }
