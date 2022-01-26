beea72401ae2744ad129181c21309657ee47c38f
FERRER Simon Ext INNOV/IT-S <simon.ferrer.ext@orange.com>
2022-01-26 14:48:38 +0100

    card align

--- src/fr/web/exemples-de-composants/tooltips/script.js
+++ src/fr/web/exemples-de-composants/tooltips/script.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-	$(function () {
-	$('[data-bs-toggle="tooltips"]').tooltip()
+document.addEventListener("DOMContentLoaded", function(event) {
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  		return new boosted.Tooltip(tooltipTriggerEl)
 	})
+
 });
