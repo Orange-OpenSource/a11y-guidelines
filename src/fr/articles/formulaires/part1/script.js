472595dab411e40d9d2ba019e799728bf0bf7af6
FERRER Simon Ext INNOV/IT-S <simon.ferrer.ext@orange.com>
2022-11-04 10:28:12 +0100

    form part1

---
+++ src/fr/articles/formulaires/part1/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("password_visibility").onclick = function (e){
+        let password = document.getElementById("password");
+        console.log(this)
+        if(password.type=="password"){
+            password.type="text";
+            this.title ="Cacher le mot de passe";
+        }
+        else{
+            password.type="password";
+            this.title ="Afficher le mot de passe";
+        }
+    }
+
+    document.getElementById("password_visibility_final").onclick = function (e){
+        let password = document.getElementById("password_final");
+        console.log(this)
+        if(password.type=="password"){
+            password.type="text";
+            this.title ="Cacher le mot de passe";
+        }
+        else{
+            password.type="password";
+            this.title ="Afficher le mot de passe";
+        }
+    }
+})
