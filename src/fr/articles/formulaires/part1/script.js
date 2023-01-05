5098b29efb0b812f9224b79f87b29da63746f922
FERRER Simon Ext INNOV/IT-S <simon.ferrer.ext@orange.com>
2023-01-05 13:55:33 +0100

    add part 2 form

--- src/fr/articles/formulaires/part1/script.js
+++ src/fr/articles/formulaires/part1/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("password_visibility").onclick = function (e){
         let password = document.getElementById("password");
-        console.log(this)
         if(password.type=="password"){
             password.type="text";
             this.title ="Cacher le mot de passe";
@@ -14,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.getElementById("password_visibility_final").onclick = function (e){
         let password = document.getElementById("password_final");
-        console.log(this)
         if(password.type=="password"){
             password.type="text";
             this.title ="Cacher le mot de passe";
