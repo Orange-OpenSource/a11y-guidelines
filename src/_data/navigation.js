27851d8c937eb848cb4a02708069ebb25f132f8a
Pierre-Yves Ayoul <pierre-yves.ayoul@orange.com>
2024-12-10 14:54:04 +0100

    update

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -131,7 +131,7 @@ module.exports = {
           { label: 'Tester', href: '/fr/web/tester/' },
           { label: 'Auditer', href: '/fr/web/audit-wcag/' },
           { label: 'Boite à outils', href: '/fr/web/outils/' },
-          { label: 'Auto-evaluation', href: '/fr/web/checklist-initiale/' }
+          { label: 'Auto-évaluation', href: '/fr/web/checklist-initiale/' }
         ]
       },
       {
