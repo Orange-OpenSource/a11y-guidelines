768f656bd5791d00b6ad3ef119098bb1622fdae0
Pierre-Yves Ayoul <105045667+pya35@users.noreply.github.com>
2024-12-10 15:18:52 +0100

    remove tag and profile

--- src/assets/test-android.js
+++ src/assets/test-android.js
@@ -251,7 +251,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             headingTheme=currentRefTests[i].themes;
             htmlrefTests +='<h2 id="test-'+formatHeading(currentRefTests[i].themes)+'"'+htmlrefTestsClass+'>'+currentRefTests[i].themes+'</h2>';
           }
-          htmlrefTests += '<article class="accordion-item"><div class="accordion-header" id="heading'+i+'"><h3 class="card-title mb-0"><button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><span class="accordion-title h6 mb-0 me-2 flex-grow-1">' + currentRefTests[i].title + '</span><span class="badge bg-light me-2 align-self-center">'+((currentRefTests[i].profils[0] == 'Concepteur')||(currentRefTests[i].profils[0] == 'Designer')  ? translate('conception') : translate('development'))+'</span></a></h3>';
+          htmlrefTests += '<article class="accordion-item"><div class="accordion-header" id="heading'+i+'"><h3 class="card-title mb-0"><button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><span class="accordion-title h6 mb-0 me-2 flex-grow-1">' + currentRefTests[i].title + '</span></a></h3>';
 
           htmlrefTests += '</div><div id="collapse'+i+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+i+'">';
           htmlrefTests += '<div class="card-block"><div class="row">';
@@ -299,11 +299,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
             htmlrefTests += '<p>' + currentRefTests[i].exception + '</p> ';
             htmlrefTests += '</div>';
             htmlrefTests += '</div>';
-          }
-          htmlrefTests += '</div><div class="card-footer text-muted"><p><b>' + translate('profiles') + ': </b>';
-          for (let j in currentRefTests[i].profils) {
-            htmlrefTests += currentRefTests[i].profils[j];
-            j != ((currentRefTests[i].profils).length-1) ? htmlrefTests +=',  ' : '';
           }
           htmlrefTests += '</p>';
           htmlrefTests += '</div>';
