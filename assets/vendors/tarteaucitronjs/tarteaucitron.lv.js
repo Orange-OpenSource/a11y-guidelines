b638a3335b8770e1b772b82a5a70aed062c9e2f5
MewenLeHo <MewenLeHo@users.noreply.github.com>
2026-01-23 09:56:40 +0000

    deploy: f3a9002af41e27686ed30055429821a63ecdf20d

---
+++ assets/vendors/tarteaucitronjs/tarteaucitron.lv.js
@@ -0,0 +1,95 @@
+/*global tarteaucitron */
+/* min ready */
+tarteaucitron.lang = {
+    "middleBarHead": "☝️ 🍪",
+    "adblock": "Sveiks! Šī vietne ir pārskatāma un ļauj tev izvēlēties, kuri trešo pušu pakalpojumi tiek izmantoti.",
+    "adblock_call": "Lūdzu, izslēdz savu reklāmu bloķētāju, lai sāktu pielāgošanu.",
+    "reload": "Pārlādēt lapu",
+
+    "alertBigScroll": "Turpinot ritināšanu,",
+    "alertBigClick": "Turpinot pārlūkot šo lapu,",
+    "alertBig": "tu piekrīti visu trešo pušu pakalpojumu izmantošanai.",
+
+    "alertBigPrivacy": "Lai parādītu šīs vietnes saturu un uzlabotu lietotāja pieredzi, mēs izmantojam sīkfailus",
+    "alertSmall": "Pārvaldīt pakalpojumus",
+    "personalize": "Iestatījumi",
+    "acceptAll": "OK, piekrītu",
+    "close": "Aizvērt",
+    "closeBanner": "Slēpt sīkfailu reklāmkarogu",
+
+    "privacyUrl": "Privātuma politika",
+
+    "all": "Dod priekšroku visiem pakalpojumiem",
+
+    "info": "Tava personas datu aizsardzība",
+    "disclaimer": "Atļaujot trešo pušu pakalpojumus, tu piekrīti sīkfailu un izsekošanas tehnoloģiju izmantošanai, kas nepieciešamas to darbībai.",
+    "allow": "Piekrītu",
+    "deny": "Noraidu",
+    "noCookie": "Šis pakalpojums neizmanto sīkfailus.",
+    "useCookie": "Šis pakalpojums var instalēt sīkfailus.",
+    "useCookieCurrent": "Šis pakalpojums ir instalējis sīkfailus.",
+    "useNoCookie": "Šis pakalpojums nav instalējis sīkfailus.",
+    "more": "Uzzināt vairāk",
+    "source": "Apskatīt oficiālo vietni",
+    "credit": "Sīkfailu pārvaldnieks: tarteaucitron.js",
+    "noServices": "Šī vietne neizmanto sīkfailus, kuriem nepieciešama tava piekrišana.",
+
+    "toggleInfoBox": "Rādīt / slēpt informāciju par sīkfailu glabāšanu",
+    "title": "Sīkfailu iestatījumi",
+    "cookieDetail": "Sīkfailu dati šim pakalpojumam:",
+    "ourSite": "mūsu vietnē",
+    "modalWindow": "(uznirstošais logs)",
+    "newWindow": "(jauns logs)",
+    "allowAll": "Piekrītu visiem",
+    "denyAll": "Noraidu visus",
+
+    "icon": "Sīkfaili",
+
+    "fallback": "atspējots.",
+    "allowed": "atļauts",
+    "disallowed": "nav atļauts",
+
+    "ads": {
+        "title": "Reklāmas tīkli",
+        "details": "Reklāmas tīkli var gūt ienākumus, pārdodot reklāmas vietas šajā tīmekļa vietnē"
+    },
+    "analytic": {
+        "title": "Apmeklējuma analīze",
+        "details": "Analītikas pakalpojumi palīdz veidot statistiku vietnes uzlabošanai."
+    },
+    "social": {
+        "title": "Sociālie tīkli",
+        "details": "Sociālie tīkli var uzlabot vietnes lietojamību un veicināt tās popularitāti, ļaujot dalīties saturā."
+    },
+    "video": {
+        "title": "Video",
+        "details": "Video koplietošanas pakalpojumi sniedz multivides saturu un palielina vietnes redzamību."
+    },
+    "comment": {
+        "title": "Komentāri",
+        "details": "Komentēšanas pakalpojumi atvieglo komentāru ievadi un palīdz cīnīties pret surogātpastu."
+    },
+    "support": {
+        "title": "Atbalsts",
+        "details": "Atbalsta pakalpojumi ļauj sazināties ar vietnes komandu un palīdzēt tās uzlabošanā."
+    },
+    "api": {
+        "title": "API",
+        "details": "API tiek izmantoti skriptu ielādei: ģeolokācija, meklētāji, tulkojumi..."
+    },
+    "other": {
+        "title": "Citi",
+        "details": "Pakalpojumi tīmekļa satura parādīšanai."
+    },
+
+    "google": {
+        "title": "Specifiska piekrišana Google pakalpojumiem",
+        "details": "Google var izmantot tavu informāciju auditorijas analīzei, reklāmu veiktspējas mērīšanai vai personalizētai reklāmai."
+    },
+
+    "mandatoryTitle": "Obligātie sīkfaili",
+    "mandatoryText": "Lai parādītu vietnes saturu un nodrošinātu lietotāja pieteikšanos, mēs izmantojam sīkfailus, kurus nevar atspējot.",
+
+    "save": "Saglabāt",
+    "ourpartners": "Mūsu partneri"
+};
